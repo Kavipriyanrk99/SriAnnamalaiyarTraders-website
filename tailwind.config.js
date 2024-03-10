fc111e35dea9e8ad8397311cd7e8a06db9127798
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-03-10 15:58:01 +0530

    font modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./build/*.html', './build/js/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'serif': ['"Noto Serif"', ...defaultTheme.fontFamily.serif],
+        'sans': ['"Oswald"', ...defaultTheme.fontFamily.sans],
+        'openSans': ['"Open Sans"', ...defaultTheme.fontFamily.sans] 
+      }
+    },
   },
   plugins: [],
 }
