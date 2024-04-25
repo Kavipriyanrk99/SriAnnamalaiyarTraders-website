6e45e4fbc0984a25361c82a32fb1d1f254e171f9
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-04-25 22:16:19 +0530

    Db connection corrections made

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,31 +2,16 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-            host: process.env.DATABASE_OST,
+            host: process.env.DATABASE_HOST,
             user: process.env.DATABASE_USER,
             database: process.env.DATABASE_NAME,
             port: process.env.DATABASE_PORT,
             password: process.env.DATABASE_PASSWORD,
         });
 
-// const getPool = async() => {
-//     try{
-//         const connection = await pool.getConnection();
-//         connection.release();
-
-//         console.log('MySQL connection pool created');
-
-//         return pool;
-//     } catch(error){
-//         console.log(error);
-//     }
-// }
-
 const getPool = async() => {
     const connection = await pool.getConnection();
     connection.release();
-
-    console.log('MySQL connection pool created');
 
     return pool;
 }
