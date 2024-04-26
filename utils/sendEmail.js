05a536dec2198a2cab429746ab219a5ecf2e9929
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-04-26 22:58:12 +0530

    email sender configured using nodemailer

---
+++ utils/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: process.env.SERVICE,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    } 
+});
+
+const sendEmail = async(subject, body) => {
+    try{
+        const info = await transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to: process.env.SMTP_TO_ADDRESS,
+            subject: subject,
+            text: body
+        });
+
+        console.log(`Message sent: ${info.messageId}`);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+module.exports = { sendEmail }
