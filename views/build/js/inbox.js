9831cbb676c49daa545269a0317d0f5a4fe0651f
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-05-05 20:39:17 +0530

    items reversed

--- views/build/js/inbox.js
+++ views/build/js/inbox.js
@@ -32,7 +32,7 @@ const initApp = async() => {
                 <p class="md:w-3/4 w-full text-left text-sm text-gray-500 mb-3 line-clamp-2">${item.email_message}</p>
                 <div class="flex justify-between items-center">
                     <a href="mailto:${item.email_address}" class="font-sourceSans font-semibold text-left text-blue-700 text-sm"><i class="fa-solid fa-envelope mr-2"></i>${item.email_address}</a>
-                    <a href="/admin/${item.id}" class="px-3 py-1 font-sourceSans font-semibold text-white bg-blue-700 rounded-2xl active:bg-blue-600">view <i class="fa-regular fa-eye text-sm"></i></a>
+                    <a href="/admin/inbox/${item.id}" class="px-3 py-1 font-sourceSans font-semibold text-white bg-blue-700 rounded-2xl active:bg-blue-600">view <i class="fa-regular fa-eye text-sm"></i></a>
                 </div>
             </article>`;
 
